185cbdf751c8f3e459759bec5804bc7a81cf3ebf
Ratimir <ra0953029364@gmail.com>
2019-12-07 19:40:33 +0200

    Добавьте файлы проекта.

---
+++ exercise_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите число ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine($"{i}*{n}={i * n}");
+            }
+            int j = 0;
+            while (n != 0)
+            {
+                j++;
+                n /= 10;
+
+            }
+            Console.WriteLine($"Количество разрядов введенного числа : {j}");
+                Console.ReadKey();
+            
+
+        }
+    }
+}
